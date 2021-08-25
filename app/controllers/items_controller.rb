5d07286294cfd4874bd2cab81ebc41c50810caed
Mitsusatonambu <nammitsu1993@gmail.com>
2021-08-25 12:15:36 +0900

    ActiveHash ActiveStorage バリデーション　itemsコントローラーのnewアクション定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,7 @@ class ItemsController < ApplicationController
   def index
   end
 
+  def new
+    @items = Item.all
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,15 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
+
+  validates :name, :info, :price, presence: true
+  validates :category_id, :delivery_date_id, :delivery_fee_id, :prefecture_id, :status_id, numericality: { other_than: 1 , message: "can't be blank"}
+
+  extend Activehash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :delivery_date
+  belongs_to :delivery_fee
+  belongs_to :prefecture
+  belongs_to :status
+
 end
