b1df4dfff8f98b62c1a952f2467bb1b62ae51b70
Mitsusatonambu <nammitsu1993@gmail.com>
2021-08-27 16:29:16 +0900

    修正2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_index, only: [:edit, :update]
   before_action :item_find, only: [:show, :edit, :update]
+  before_action :move_to_index, only: [:edit, :update]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -42,14 +42,13 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:image, :name, :info, :category_id, :status_id, :delivery_fee_id, :delivery_date_id, :prefecture_id, :price).merge(user_id: current_user.id)
   end
 
-  def move_to_index 
+  def item_find
     @item = Item.find(params[:id])
+  end
+
+  def move_to_index 
     unless @item.user_id == current_user.id
       redirect_to action: :index
     end
-  end
-
-  def item_find
-    @item = Item.find(params[:id])
   end
 end
